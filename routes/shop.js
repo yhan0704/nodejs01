7c6f17c98e8ec84b72befb3ca17f43d08664e9ea
Young <hanyc1205@gmail.com>
2020-08-14 11:23:40 -0400

    conncect to views to routes

---
+++ routes/shop.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.sendFile("../views/shop.html")
+});
+
+module.exports = router;
